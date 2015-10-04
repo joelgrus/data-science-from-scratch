fdac5a68affef6d19de2954ba051c70cfb68b6a8
Pandeesh <pandeesh@gmail.com>
2015-10-03 21:32:47 -0700

    Update introduction.py
    importing matplotlib since it's not loaded by default

--- code/introduction.py
+++ code/introduction.py
@@ -130,6 +130,8 @@ salaries_and_tenures = [(83000, 8.7), (88000, 8.1),
                         (69000, 6.5), (76000, 7.5),
                         (60000, 2.5), (83000, 10),
                         (48000, 1.9), (63000, 4.2)]
+                        
+from matplotlib import pyplot as plt  # not loaded by default                      
 
 def make_chart_salaries_by_tenure(plt):
     tenures = [tenure for salary, tenure in salaries_and_tenures]
