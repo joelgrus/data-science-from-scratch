c42086dd293fcb464090b500db9d70ab19ff8e67
Christian Clauss <cclauss@me.com>
2020-06-20 18:12:36 +0200

    Make partial_difference_quotient() visible to estimate_gradient()
    If partial_difference_quotient() is indented under main() the is not in scope to be called from within estimate_gradient().

--- scratch/gradient_descent.py
+++ scratch/gradient_descent.py
@@ -11,6 +11,16 @@ def difference_quotient(f: Callable[[float], float],
                         h: float) -> float:
     return (f(x + h) - f(x)) / h
 
+def partial_difference_quotient(f: Callable[[Vector], float],
+                                    v: Vector,
+                                    i: int,
+                                    h: float) -> float:
+        """Returns the i-th partial difference quotient of f at v"""
+        w = [v_j + (h if j == i else 0)    # add h to just the ith element of v
+             for j, v_j in enumerate(v)]
+    
+        return (f(w) - f(v)) / h
+
 def square(x: float) -> float:
     return x * x
 
@@ -75,21 +85,9 @@ def main():
     plt.plot(xs, estimates, 'b+', label='Estimate')   # blue +
     plt.legend(loc=9)
     # plt.show()
-    
-    
     plt.close()
-    
-    def partial_difference_quotient(f: Callable[[Vector], float],
-                                    v: Vector,
-                                    i: int,
-                                    h: float) -> float:
-        """Returns the i-th partial difference quotient of f at v"""
-        w = [v_j + (h if j == i else 0)    # add h to just the ith element of v
-             for j, v_j in enumerate(v)]
-    
-        return (f(w) - f(v)) / h
-    
-    
+
+
     # "Using the Gradient" example
     
     # pick a random starting point
